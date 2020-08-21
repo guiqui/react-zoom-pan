0ea70d11b2110d1ece09cace7b1d7fa47f005c50
guillermo <guillermo@uxxu.io>
2020-08-21 14:14:56 +1200

    Improve demo

--- src/demo/pages/App.js
+++ src/demo/pages/App.js
@@ -27,34 +27,41 @@ class App extends Component {
     return (
       <div className="app-container">
         <div className="title-container">
-          <h1>Getting Started Demo</h1>
+          <h2 style={{ flex: 1 }}>React Zoom Pan Demo </h2>
+          <a href="http://uxxu.io">
+            <img style={{ width: 120, margin: 10 }} draggable="false" src="https://uxxu.io/images/logo.png" />
+          </a>
         </div>
         <div className="flow-container">
           <Flow>
             <div className="viewport-element" x={50} y={50} h={100} w={200}>
               <input type="text" />
             </div>
-            <div className="viewport-element" w={100} h={200}>
+            <div className="viewport-element" x={400} y={300} w={700} h={200}>
               <img
                 draggable="false"
                 src="https://specials-images.forbesimg.com/imageserve/5d35eacaf1176b0008974b54/960x0.jpg?cropX1=790&cropX2=5350&cropY1=784&cropY2=3349"
               />
             </div>
-            <div className="viewport-element" w={100} h={200}>
-              guillermo2
+            <div className="viewport-element" x={100} y={300} w={200} h={200}>
+              <img
+                draggable="false"
+                style={{ width: '100%', height: '100%' }}
+                src="https://s.gravatar.com/avatar/e2e674b507196e6b07241b3a5d2ac166?size=496&default=retro"
+              />
             </div>
-            <div className="viewport-element" w={100} h={200}>
-              guillermo2
+            <div className="viewport-element container1" x={800} y={50} w={200} h={50}>
+              container
             </div>
-            <div className="viewport-element" w={100} h={200}>
+            <div className="viewport-element" x={400} y={600} w={100} h={200}>
               <div>1</div>
               <div>2</div>
             </div>
-            <div className="viewport-element" x={50} y={50} h={100} w={200}>
+            <div className="viewport-element" x={400} y={50} h={150} w={200}>
               <input type="text" />
               <input type="text" />
               <input type="text" />
-              <button>Hi</button>
+              <button>Do it!</button>
             </div>
           </Flow>
         </div>
